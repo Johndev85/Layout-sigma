0e530396e19cd87c0d01eb7fc5f7bf12b2ef1583
John Botero <jhondark@gmail.com>
2020-08-11 17:04:32 -0500

    departments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-fetch("https://sigma-studios.s3-us-west-2.amazonaws.com/test/colombia.json")
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-    })
+const $departamento = document.getElementById("departamento")
+const $ciudad = document.getElementById("ciudad")
+
+const departamentos = () => {
+    const indice = $departamento.selectedIndex
+    console.log(indice)
+    const opcionSeleccionada = $departamento.options[indice]
+    console.log(opcionSeleccionada.text)
+    // mostrarCiudades(opcionSeleccionada)
+}
+
+const mostrarCiudades = () => {
+    const option = document.createElement("option")
+    const valor = new Date().getTime()
+    option.value = valor
+    option.text = valor
+    select.appendChild(option)
+}
+
+$departamento.addEventListener("change", departamentos)
+
+// fetch("http://localhost:3000/Amazonas")
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data)
+//         return data
+//     })
